3803a949031551e39a5d1e3f9b9ef150b60e7c84
FcAYH <1473988037@qq.com>
2023-02-07 11:04:10 +0800

    修改了命名空间

---
+++ ImageUpdateTool/Utils/CommandRunner.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace ImageUpdateTool.Utils;
+
+internal class CommandRunner
+{
+    public string ExecutablePath { get; }
+    public string WorkingDirectory { get; }
+
+    public CommandRunner(string executablePath, string? workingDirectory = null)
+    {
+        ExecutablePath = executablePath ?? throw new ArgumentNullException(nameof(executablePath));
+        WorkingDirectory = workingDirectory ?? Path.GetDirectoryName(executablePath);
+    }
+
+    public string Run(string arguments)
+    {
+        var info = new ProcessStartInfo(ExecutablePath, arguments)
+        {
+            CreateNoWindow = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            WorkingDirectory = WorkingDirectory,
+        };
+        var process = new Process
+        {
+            StartInfo = info,
+        };
+        process.Start();
+        process.WaitForExit();
+        if (process.ExitCode == 0)
+            return "";
+                
+        return process.StandardError.ReadToEnd();
+    }
+}
---
+++ ImageUpdateTool/Utils/Enums.cs
@@ -0,0 +1,11 @@
+﻿namespace ImageUpdateTool.Utils;
+
+enum ImageExtension
+{
+    png,
+    jpeg,
+    gif,
+    tiff,
+    bmp,
+    jpg,
+}
