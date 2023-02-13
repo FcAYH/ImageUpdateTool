73ffebc3908a826a3a9d896046dfb299e4ec007b
FcAYH <1473988037@qq.com>
2023-02-13 14:38:49 +0800

    新增了检测git是否存在，在未安装git的机子上会有无法使用的弹窗。

--- ImageUpdateTool/AppShell.xaml.cs
+++ ImageUpdateTool/AppShell.xaml.cs
@@ -23,7 +23,7 @@ public partial class AppShell : Shell
 		ChooseStartPage();
     }
 
-	private void ChooseStartPage()
+	private async void ChooseStartPage()
 	{
 		if (AppSettings.LocalStoragePath == string.Empty
 			|| AppSettings.GitURL == string.Empty) 
@@ -35,11 +35,41 @@ public partial class AppShell : Shell
 			UnlockMainPage();
 			CurrentItem = MainPageItem;
 		}
-	}
 
-	public void UnlockMainPage()
+#if WINDOWS
+		string gitKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Git_is1";
+        Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(gitKey);
+
+		if (key == null)
+		{
+			await DisplayAlert("缺失Git!", "未在计算机中检测到Git程序，请确保安装Git，并将Git路径放入环境变量中。", "OK");
+			CurrentItem = AboutPageItem;
+			LockPages();
+		}
+
+		string path = Environment.GetEnvironmentVariable("Path");
+		if (!path.Contains("Git\\cmd"))
+		{
+            await DisplayAlert("环境变量缺失", "请将Git路径放入环境变量中。", "OK");
+            CurrentItem = AboutPageItem;
+            LockPages();
+        }
+#endif
+    }
+
+    public void UnlockMainPage()
 	{
         MainPageItem.IsEnabled = true;
         MainPageItem.IsVisible = true;
     }
+
+	private void LockPages() 
+	{
+        MainPageItem.IsEnabled = false;
+        MainPageItem.IsVisible = false;
+
+		SettingsPageItem.IsEnabled = false;
+		SettingsPageItem.IsVisible = false;
+    }
 }
+
