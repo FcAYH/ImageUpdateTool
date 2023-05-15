44c8d37a31815e6c5208d00bb4ed8b5e38d63d8a
FcAYH <1473988037@qq.com>
2023-05-15 23:29:05 +0800

    测试了图片预览功能

--- ImageUpdateTool/Pages/TechTest.xaml.cs
+++ ImageUpdateTool/Pages/TechTest.xaml.cs
@@ -13,38 +13,18 @@ public partial class TechTest : ContentPage
     public TechTest()
     {
         InitializeComponent();
+        var baseColor = this.BackgroundColor;
+        Color gridColor = new Color(baseColor.Red, baseColor.Green, baseColor.Blue, 0.8f);
+        ImageGrid.BackgroundColor = gridColor;
     }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        // 创建一个半透明的容器
-        var overlay = new BoxView
-        {
-            Color = Color.FromRgba(0, 0, 0, 0.5),
-        };
+        ImageGrid.IsVisible = true;
+    }
 
-        grid.Add(overlay);
-        grid.SetRow(overlay, 0);
-        grid.SetColumn(overlay, 0);
-        grid.SetRowSpan(overlay, 2);
-
-        // 创建一个返回按钮
-        var backButton = new Button
-        {
-            Text = "Back",
-            HorizontalOptions = LayoutOptions.Center,
-            VerticalOptions = LayoutOptions.Center
-        };
-
-        grid.Add(backButton);
-        grid.SetRow(backButton, 0);
-        grid.SetColumn(backButton, 0);
-
-        // 为返回按钮添加点击事件处理器，移除容器和按钮
-        backButton.Clicked += (s, e) =>
-        {
-            grid.Remove(overlay);
-            grid.Remove(backButton);
-        };
+    private void CloseButton_Clicked(object sender, EventArgs e)
+    {
+        ImageGrid.IsVisible = false;
     }
 }
